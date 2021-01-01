0357e90c1e2a0bac782253e3530f14b396bc9463
allan <allan.w.hart@gmail.com>
2021-01-01 09:33:52 -0700

    Cleanup

--- plugins/gatsby-theme-app-layout/gatsby-ssr.js
+++ plugins/gatsby-theme-app-layout/gatsby-ssr.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import AppThemeProvider from './AppThemeProvider';
 
-export const wrapRootElement = ({ element }) =>
+export const wrapRootElement = ({ element }) => (
 	<AppThemeProvider>
-		{element}
-	</AppThemeProvider>;
+    {element}
+	</AppThemeProvider>
+);
 
 wrapRootElement.propTypes = {
-	element: PropTypes.node
+  element: PropTypes.node
 };
